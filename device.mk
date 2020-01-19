9d021b32a86d8b0ad6087bc57dd7338ebc6abe55
Dyneteve <dyneteve@pixelexperience.org>
2020-01-19 14:59:43 +0200

    violet: Remove modem assertion
    * Also flash firmware and a custom modem every time
    
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/violet/violet-vendor.mk)
 
+# Firmware
+$(call inherit-product, vendor/xiaomi/violet/firmware/firmware.mk)
+
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
@@ -359,10 +362,6 @@ PRODUCT_PACKAGES += \
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
-
-# Recovery
-PRODUCT_PACKAGES += \
-    librecovery_updater_xiaomi
 
 # RIL
 PRODUCT_PACKAGES += \
