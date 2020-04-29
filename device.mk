04a4743116c8569f2335c4856fefd4405ba6235b
MikdadAli Momin <mikdad.momin121@gmail.com>
2020-04-29 22:39:11 +0530

    added anx camera config

--- device.mk
+++ device.mk
@@ -11,7 +11,7 @@ $(call inherit-product-if-exists, vendor/xiaomi/violet/violet-vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # MiuiCamera
-$(call inherit-product-if-exists, vendor/MiuiCamera/config.mk)
+$(call inherit-product-if-exists, vendor/ANXCamera/config.mk)
 
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
